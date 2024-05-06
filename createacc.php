47a2b282e2e475d4e4ceb5215ff5a808435b0ac9
Minidu <misterminidu@gmail.com>
2024-05-07 00:38:23 +0530

    "Replaced specific DB connection details with included config; started using PDO"
    This commit removes hardcoded database connection details and instead includes them from an external config file. Additionally, it appears to have switched from the `mys

--- createacc.php
+++ createacc.php
@@ -1,18 +1,7 @@
 <?php
 session_start();
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
-}
+include "src/config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
